813373a9580c969759a5bf2199b5032664cc778c
ramneekkaur <ramneek983@gmail.com>
2020-05-24 17:59:40 +0530

    doing commit

---
+++ social/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from social import models
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+class Wall(LoginRequiredMixin ,  ListView):
+    queryset=models.Post.objects.all()
+    context_object_name = 'posts'
+    template_name='social/wall.html'
+    login_url='auth/login'
